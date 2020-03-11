c9e6a489297890f24e383a0f2903ae283df4921a
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-03-11 17:03:18 +0100

    Routerfunction (#7)
    * Add react router
    
    * Fix folder strcuture
    
    * Fix import paths in app
    
    * Implement routing from one page to another
    
    * Fix svg folder placement
    
    * Fix svg folder placement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Add from "./components/pages/Add";
+import Vote from "./components/pages/Vote";
+import Result from "./components/pages/Result";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <main>
+        <Switch>
+          <Route path="/result">
+            <Result />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/vote">
+            <Vote />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
