cdd846d58c8b26a817815b01e89bfffc39f27cd3
Caruki <caruki91@posteo.de>
2020-03-12 17:29:33 +0100

    Fetch api poll results

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import Card from '../components/Card';
 import RedirectButton from '../components/RedirectButton';
+import { useParams } from 'react-router-dom';
+
+const pollApiURL =
+  process.env.REACT_APP_POLLS_API ||
+  'https://my-json-server.typicode.com/Caruki/quest-app/polls';
 
 export default function Result() {
+  const { pollId } = useParams();
+  const [poll, setPoll] = React.useState();
+
+  React.useEffect(() => {
+    async function getPoll() {
+      const response = await fetch(`${pollApiURL}/${pollId}`);
+      const poll = await response.json();
+      setPoll(poll);
+    }
+    getPoll();
+  }, [pollId]);
+
   return (
-    <>
-      <Card>
-        <RedirectButton
-          name="Create your own poll"
-          destination="/add"
-        ></RedirectButton>
-        <RedirectButton
-          name="Go back to overview"
-          destination="/"
-        ></RedirectButton>
-      </Card>
-    </>
+    <Card>
+      {poll?.question}
+      <RedirectButton
+        name="Create your own poll"
+        destination="/add"
+      ></RedirectButton>
+      <RedirectButton
+        name="Go back to overview"
+        destination="/"
+      ></RedirectButton>
+    </Card>
   );
 }
