b7c2dcc7122a20c1e3c5f6b97247cc4667e02860
Caruki <caruki91@posteo.de>
2020-03-12 09:40:09 +0100

    Fix mobile view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,34 +6,36 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <Card>
-      <h1 className="heading">Welcome to the Questing Beast</h1>
-      <div className="textContainer">
-        <div className="welcome">
-          <p>
-            Here you can become your very own questing beast by making polls or
-            answering other's questions.{" "}
-          </p>
-          <p>Please choose one:</p>
+    <>
+      <Card>
+        <h1 className="heading">Welcome to the Questing Beast</h1>
+        <div className="textContainer">
+          <div className="welcome">
+            <p>
+              Here you can become your very own questing beast by making polls
+              or answering other's questions.{" "}
+            </p>
+            <p>Please choose one:</p>
+          </div>
         </div>
-      </div>
-      <div className="buttonContainer">
-        <RedirectButton
-          className="navButton"
-          name="Create your own poll"
-          destination="/add"
-        ></RedirectButton>
-        <RedirectButton
-          className="navButton"
-          name="Vote in one of our polls"
-          destination="/vote"
-        ></RedirectButton>
-        <RedirectButton
-          className="navButton"
-          name="See the results of your poll"
-          destination="/result"
-        ></RedirectButton>
-      </div>
-    </Card>
+        <div className="buttonContainer">
+          <RedirectButton
+            className="navButton"
+            name="Create your own poll"
+            destination="/add"
+          ></RedirectButton>
+          <RedirectButton
+            className="navButton"
+            name="Vote in one of our polls"
+            destination="/vote"
+          ></RedirectButton>
+          <RedirectButton
+            className="navButton"
+            name="See the results of your poll"
+            destination="/result"
+          ></RedirectButton>
+        </div>
+      </Card>
+    </>
   );
 }
