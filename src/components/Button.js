d8958fa6e5c957831e7e6f9131d6f37650e0f986
Caruki <caruki91@posteo.de>
2020-03-12 14:51:33 +0100

    Fix various things
    Delete comments and fix flexbox behaviour

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,6 @@
-import React from "react";
-import "../components/Buttons.css";
+import React from 'react';
+import '../components/Buttons.css';
 
 export default function Button(props) {
-  // let returnElement = undefined
-
-  // if (props.type === "submit") {
-  //     returnElement = <input type="submit"></input>
-  // } else {
-  //     returnElement= <button className={props.className || "button"} {...props} />;
-  // }
-
-  return <button className={props.className || "button"} {...props} />;
+  return <button className={props.className || 'button'} {...props} />;
 }
