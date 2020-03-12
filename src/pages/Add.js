9bb1495ad6abe12b5cace5ed99c0435ef2cd31af
Caruki <caruki91@posteo.de>
2020-03-12 11:41:28 +0100

    Add form with input fields and submit button

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,14 +1,37 @@
 import React from "react";
-
+import "./Add.css";
 import Card from "../components/Card";
 import RedirectButton from "../components/RedirectButton";
+import Button from "../components/Button";
 
 export default function Add() {
   return (
     <>
       <Card>
-        {/*<SubmitButton></SubmitButton>*/}
+        <form className="add-form">
+          <input
+            className="add-form_input add-form_input-question"
+            type="text"
+            placeholder="Enter your question"
+          />
+          <input
+            className="add-form_input add-form_input-answer"
+            type="text"
+            placeholder="First answer option"
+          />
+          <input
+            className="add-form_input add-form_input-answer"
+            type="text"
+            placeholder="Second answer option"
+          />
+          <input
+            className="add-form_input add-form_input-answer"
+            type="text"
+            placeholder="Third answer option"
+          />
 
+          <Button type="submit">Create Poll</Button>
+        </form>
         <RedirectButton
           name="Go to voting"
           destination="/vote"
