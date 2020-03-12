d9003fbcb3d530924d950924c3fe8b662f5303d0
Caruki <caruki91@posteo.de>
2020-03-12 12:27:33 +0100

    Add input value and styling to form

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -5,41 +5,77 @@ import RedirectButton from "../components/RedirectButton";
 import Button from "../components/Button";
 
 export default function Add() {
+  const [question, setQuestion] = React.useState("");
+  const [optionOne, setOptionOne] = React.useState("");
+  const [optionTwo, setOptionTwo] = React.useState("");
+  const [optionThree, setOptionThree] = React.useState("");
+
+  const poll = {
+    question: question,
+    optionOne: optionOne,
+    optionTwo: optionTwo,
+    optionThree: optionThree
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(poll));
+  }
   return (
     <>
       <Card>
-        <form className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
           <input
             className="add-form_input add-form_input-question"
             type="text"
             placeholder="Enter your question"
+            value={question}
+            onChange={event => {
+              setQuestion(event.target.value);
+            }}
           />
           <input
             className="add-form_input add-form_input-answer"
             type="text"
             placeholder="First answer option"
+            value={optionOne}
+            onChange={event => {
+              setOptionOne(event.target.value);
+            }}
           />
           <input
             className="add-form_input add-form_input-answer"
             type="text"
             placeholder="Second answer option"
+            value={optionTwo}
+            onChange={event => {
+              setOptionTwo(event.target.value);
+            }}
           />
           <input
             className="add-form_input add-form_input-answer"
             type="text"
             placeholder="Third answer option"
+            value={optionThree}
+            onChange={event => {
+              setOptionThree(event.target.value);
+            }}
           />
 
-          <Button type="submit">Create Poll</Button>
+          <Button type="submit">
+            <strong>Create Poll</strong>
+          </Button>
         </form>
-        <RedirectButton
-          name="Go to voting"
-          destination="/vote"
-        ></RedirectButton>
-        <RedirectButton
-          name="See results of this poll"
-          destination="/result"
-        ></RedirectButton>
+        <div className="redirect-container">
+          <RedirectButton
+            name="Go to voting"
+            destination="/vote"
+          ></RedirectButton>
+          <RedirectButton
+            name="See results of this poll"
+            destination="/result"
+          ></RedirectButton>
+        </div>
       </Card>
     </>
   );
