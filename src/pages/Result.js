984d95e9fe833c0ea26d1120f5925fc0d0707d55
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-03-19 14:05:59 +0100

    Outsourcing (#21)
    * Outsource polls api and getPoll
    
    * Outsource patchPoll
    
    * Optimize add page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,22 +3,14 @@ import Card from '../components/Card';
 import RedirectButton from '../components/RedirectButton';
 import { Form, FormQuestion, FormResultAnswer } from '../components/Form';
 import { useParams } from 'react-router-dom';
-
-const pollApiURL =
-  process.env.REACT_APP_POLLS_API ||
-  'https://my-json-server.typicode.com/Caruki/quest-app/polls';
+import { getPoll } from '../api/polls';
 
 export default function Result() {
   const { pollId } = useParams();
   const [poll, setPoll] = React.useState();
 
   React.useEffect(() => {
-    async function getPoll() {
-      const response = await fetch(`${pollApiURL}/${pollId}`);
-      const poll = await response.json();
-      setPoll(poll);
-    }
-    getPoll();
+    getPoll(pollId).then(poll => setPoll(poll));
   }, [pollId]);
 
   return (
