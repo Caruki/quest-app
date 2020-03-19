dc8fdd3050073b3a9e306f6fef3fa2d5382fb52d
Caruki <caruki91@posteo.de>
2020-03-19 19:00:01 +0100

    Add loading animation

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -4,15 +4,23 @@ import RedirectButton from '../components/RedirectButton';
 import { Form, FormQuestion, FormResultAnswer } from '../components/Form';
 import { useParams } from 'react-router-dom';
 import { getPoll } from '../api/polls';
+import LoadingAnimation from '../components/LoadingAnimation';
 
 export default function Result() {
   const { pollId } = useParams();
   const [poll, setPoll] = React.useState();
+  const [isLoadingGetPoll, setIsLoadingGetPoll] = React.useState(true);
 
   React.useEffect(() => {
-    getPoll(pollId).then(poll => setPoll(poll));
+    setIsLoadingGetPoll(true);
+    getPoll(pollId)
+      .then(poll => setPoll(poll))
+      .then(() => setIsLoadingGetPoll(false));
   }, [pollId]);
 
+  if (isLoadingGetPoll) {
+    return <LoadingAnimation />;
+  }
   return (
     <Card>
       <Form>
--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -5,19 +5,26 @@ import { Form, FormQuestion } from '../components/Form';
 import Button from '../components/Button';
 import RadioInput from '../components/RadioInput';
 import { getPoll, patchPoll } from '../api/polls';
+import LoadingAnimation from '../components/LoadingAnimation';
 
 export default function Vote() {
   const { pollId } = useParams();
   const history = useHistory();
   const [poll, setPoll] = React.useState(null);
   const [answer, setAnswer] = React.useState(null);
+  const [isLoadingPatchPoll, setIsLoadingPatchPoll] = React.useState(false);
+  const [isLoadingGetPoll, setIsLoadingGetPoll] = React.useState(true);
 
   React.useEffect(() => {
+    setIsLoadingGetPoll(true);
     getPoll(pollId).then(poll => setPoll(poll));
+    setIsLoadingGetPoll(false);
   }, [pollId]);
 
   async function handleSubmit(event) {
     event.preventDefault();
+
+    setIsLoadingPatchPoll(true);
 
     const updatedPoll = { ...poll };
     updatedPoll.votes.push(answer);
@@ -25,24 +32,30 @@ export default function Vote() {
     await patchPoll(pollId, updatedPoll);
     history.push(`/polls/${poll.id}/result`);
   }
+
   const answerOptions = ['optionOne', 'optionTwo', 'optionThree'];
 
   return (
     <Card>
-      <Form onSubmit={handleSubmit}>
-        <FormQuestion>{poll?.question}</FormQuestion>
-        {answerOptions.map(option => (
-          <RadioInput
-            key={option}
-            checked={answer === option}
-            onChange={event => setAnswer(event.target.value)}
-            value={option}
-            label={poll?.[option]}
-            name="answer"
-          />
-        ))}
-        <Button>Vote</Button>
-      </Form>
+      {isLoadingGetPoll ? (
+        <LoadingAnimation />
+      ) : (
+        <Form onSubmit={handleSubmit}>
+          <FormQuestion>{poll?.question}</FormQuestion>
+          {answerOptions.map(option => (
+            <RadioInput
+              key={option}
+              checked={answer === option}
+              onChange={event => setAnswer(event.target.value)}
+              value={option}
+              label={poll?.[option]}
+              name="answer"
+              disabled={isLoadingPatchPoll}
+            />
+          ))}
+          <Button disabled={isLoadingPatchPoll}>Vote</Button>
+        </Form>
+      )}
     </Card>
   );
 }
